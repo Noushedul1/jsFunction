e41a130cf27da7d5af37b67ff402de277d7d8f0e
Md.Noushedul Islam <noushedakib@gmail.com>
2020-10-21 23:25:10 +0600

    i put here es5 and es6 function

---
+++ js/function.js
@@ -0,0 +1,26 @@
+function akib(){
+	return "Southern University";
+}
+console.log(akib());
+
+//perameter
+function islam(a,b){
+	return a*b;
+}
+console.log(islam(2,5));
+
+function sqr(a){
+	return a*a;
+}
+console.log(sqr(10));
+
+// ES6 function(Arrow function)
+let country = () => "Noushedul Islam BSCSE";
+console.log(country());
+
+let add = a => a+a;
+console.log(add(2));
+
+let mul = (a,b) => a*b;
+console.log(mul(2,3));
+
